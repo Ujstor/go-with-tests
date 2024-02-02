fb07e7bfed2e6fe140e0f97ee19ed8422a60da5a
Ujstor <astipan@gmail.com>
2024-02-02 13:23:56 +0100

    tails

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -10,12 +10,18 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lenghtOfNumbers := len(numbersToSum)
-	sums := make([]int, lenghtOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	var sums []int
+	for _, number := range numbersToSum {
+		sums = append(sums, Sum(number))
 	}
+	return sums
+}
 
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, number := range numbersToSum {
+		tail := number[1:]
+		sums = append(sums, Sum(tail))
+	}
 	return sums
 }
--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -28,3 +28,16 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+
+	t.Run("sums of slices tails", func (t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{3, 4})
+	want := []int{2, 4}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+	})
+
+}
