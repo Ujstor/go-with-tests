c1b029cefa5f0c313e6ab56cdd72fcdf9b09da5b
Ujstor <astipan@gmail.com>
2024-02-18 04:25:41 +0100

    add to dict

--- maps/maps.go
+++ maps/maps.go
@@ -1,19 +1,37 @@
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find word in dictionary")
+type DictionaryError string
 
-func (d Dictionary) Search(word string) (string, error){
-	definition, ok := d[word]
-		if !ok {
-			return "", ErrNotFound 
-		}
-		return definition, nil
+func (e DictionaryError) Error() string {
+	return string(e)
 }
 
-func (d Dictionary) Add(word string, definition string) {
-	d[word] = definition
+var (
+	ErrNotFound   = DictionaryError("could not find word in dictionary")
+	ErrWordExists = DictionaryError("could not add word already exists")
+)
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word string, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -20,13 +20,26 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test2"
-	definition := "this is test"
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	dictionary.Add(word, definition)
+		err := dictionary.Add(word, definition)
 
-	assertDefinitione(t, dictionary, word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 //helper functions
@@ -46,7 +59,7 @@ func assertError(t testing.TB, got, want error) {
 	}
 }
 
-func assertDefinitione(t testing.TB, dictionary Dictionary, word, definition string) {
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
 	t.Helper()
 
 	word, err := dictionary.Search(word)
