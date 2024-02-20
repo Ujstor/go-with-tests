fd956166e2c7ef53718935c55dcc8cd36485ba58
Ujstor <astipan@gmail.com>
2024-02-21 00:12:00 +0100

    Configurable Sleeper

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -9,9 +9,9 @@ import (
 
 const (
 	countdownStart = 3
-	finalWord = "Go!"
-	write = "write"
-	sleep = "sleep"
+	finalWord      = "Go!"
+	write          = "write"
+	sleep          = "sleep"
 )
 
 type Sleeper interface {
@@ -19,17 +19,29 @@ type Sleeper interface {
 }
 
 type SpyCountdownOperator struct {
-	Calls []string 
+	Calls []string
 }
 
 type DefaultSleeper struct{}
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSleep time.Duration
+}
 
 func (s *SpyCountdownOperator) Sleep() {
 	s.Calls = append(s.Calls, sleep)
 }
 
-func (s *SpyCountdownOperator) Write(p []byte) (n int, err error){
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSleep = duration
+}
+
+func (s *SpyCountdownOperator) Write(p []byte) (n int, err error) {
 	s.Calls = append(s.Calls, write)
 	return
 }
@@ -38,6 +50,9 @@ func (s *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
@@ -45,12 +60,12 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 		sleeper.Sleep()
 	}
 
-	fmt.Fprint(out, finalWord) 
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
 
-	defaultSleeper := &DefaultSleeper{}
-	Countdown(os.Stdout, defaultSleeper)
+	sleeper := &ConfigurableSleeper{5 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
 
 }
