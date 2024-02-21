a9ce8849e2a911e24d718000d0dfbcb925b607cb
Ujstor <astipan@gmail.com>
2024-02-21 22:53:15 +0100

    goroutines & chanels

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -2,11 +2,26 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, url []string) map[string]bool {
 	resault := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range url {
-		resault[url] = wc(url)
+
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
 	}
+
+	for i := 0; i < len(url); i++ {
+		r := <-resultChannel
+		resault[r.string] = r.bool
+	}
+
 	return resault
 }
